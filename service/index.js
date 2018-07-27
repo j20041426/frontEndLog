e03cef2e75a1a6dd68cb4baa6d58ca11df8baddc
hzq <13402318457@163.com>
2018-07-27 13:50:03 +0800

    启用mysql

--- service/index.js
+++ service/index.js
@@ -3,6 +3,9 @@
  * 文件说明:node服务器，入口文件
  * Creeated by hzq on 2018/7/2
  */
-const app = require('./api');
+// const app = require('./api');
+const models = require('./mySQL');
+const express = require('express');
+const app = express();
 //监听端口为5000
 app.listen(5000, () => console.log('**********【服务器启动成功】**********'));
